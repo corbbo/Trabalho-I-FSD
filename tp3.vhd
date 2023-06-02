76c9f2b0f0de5ae60b2690492dd8b1351bec31dd
Thiago Zilberknop <134622097+corbbo@users.noreply.github.com>
2023-06-02 14:26:07 -0300

    Update tp3.vhd

--- tp3.vhd
+++ tp3.vhd
@@ -1,5 +1,5 @@
 -----------------------------------------------
--- TRABALHO TP3 - THIAGO ZILBERKNOP  16/MAIO/23
+-- TRABALHO TP3 - THIAGO ZILBERKNOP  2/JUNHO/23
 -----------------------------------------------
 -- thiago.zilberknop@edu.pucrs.br -------------
 
@@ -29,92 +29,106 @@ end entity;
 architecture tp3 of tp3 is
   type state is (idle, buscando, progA, progB, progC, progD, zerando, bloqueio);
   signal EA, PE: state;
-  signal habilita : std_logic := '0';
   signal found : std_logic := '0';
   signal match : std_logic_vector(3 downto 0) := "0000";
   signal program : std_logic_vector(3 downto 0) := "0000";
+  signal sel : std_logic_vector(3 downto 0) := "0000";
+  signal data : std_logic_vector(7 downto 0) := x"00";
 begin
 
   -- REGISTRADOR DE DESLOCAMENTO QUE RECEBE O FLUXO DE ENTRADA
+  registrador : process(clock)
+  begin
+    if clock'event and clock = '1' then
+      data(6 downto 0) <= data(7 downto 1);
+      data(7) <= din;
+    end if;
+  end process registrador;
+      
 
   -- 4 PORT MAPS PARA OS compara_dado ------------------------ 
-  comp_dado_A : entity work.comp
+  comp_dado_A : entity work.compara_dado
   port map (
     clock       =>    clock,
     reset       =>    reset,
     prog        =>    program(0),
-    habilita    =>    habilita,
-    dado        =>    din,
+    habilita    =>    sel(0),
+    dado        =>    data,
     pattern     =>    padrao,
     match       =>    match(0)
   );
-  comp_dado_B : entity work.comp
+  comp_dado_B : entity work.compara_dado
   port map (
     clock       =>    clock,
     reset       =>    reset,
     prog        =>    program(1),
-    habilita    =>    habilita,
-    dado        =>    din,
+    habilita    =>    sel(1),
+    dado        =>    data,
     pattern     =>    padrao,
     match       =>    match(1)
   );
-  comp_dado_C : entity work.comp
+  comp_dado_C : entity work.compara_dado
   port map (
     clock       =>    clock,
     reset       =>    reset,
     prog        =>    program(2),
-    habilita    =>    habilita,
-    dado        =>    din,
+    habilita    =>    sel(2),
+    dado        =>    data,
     pattern     =>    padrao,
     match       =>    match(2)
   );
-  comp_dado_D : entity work.comp
+  comp_dado_D : entity work.compara_dado
   port map (
     clock       =>    clock,
     reset       =>    reset,
     prog        =>    program(3),
-    habilita    =>    habilita,
-    dado        =>    din,
+    habilita    =>    sel(3),
+    dado        =>    data,
     pattern     =>    padrao,
     match       =>    match(3)
   );
     
   found   <=  '0' when match = "0000" else 
               '1';
-  
-  --  registradores para ativar as comparações
-
-  --  registrador para o alarme interno
     
-  -- FLIP FLOP -----------------------
-  state_machine: process(clock)
+  -- FLIP FLOPS -----------------------
+  -- registro ------------------
+  registro: process(clock)
   begin
     if clock'event and clock = '1' then
       case EA is
         when progA => 
-          program(0) <= '1',
-          program(1) <= '0',
-          program(2) <= '0',
-          program(3) <= '0',
+          program(0) <= '1';
+          program(1) <= '0';
+          program(2) <= '0';
+          program(3) <= '0';
         when progB => 
-          program(0) <= '0',
-          program(1) <= '1',
-          program(2) <= '0',
-          program(3) <= '0',
+          program(0) <= '0';
+          program(1) <= '1';
+          program(2) <= '0';
+          program(3) <= '0';
         when progC =>
-          program(0) <= '0',
-          program(1) <= '0',
-          program(2) <= '1',
-          program(3) <= '0',
+          program(0) <= '0';
+          program(1) <= '0';
+          program(2) <= '1';
+          program(3) <= '0';
         when progD =>
-          program(0) <= '0',
-          program(1) <= '0',
-          program(2) <= '0',
-          program(3) <= '1',
-        when others => program <= "0000"
+          program(0) <= '0';
+          program(1) <= '0';
+          program(2) <= '0';
+          program(3) <= '1';
+        when others => program <= "0000";
        end case;
     end if;
-  end process state_machine;
+  end process registro;
+      
+  -- FSM -----------------------
+  fsm : process(clock)
+  begin
+    if clock'event and clock = '1' then
+      EA <= PE;
+    end if;
+  end process fsm;
 
   -- DECODER DE ESTADOS --------------
   decoder_states: process(EA, prog)
@@ -123,32 +137,62 @@ begin
       -- IDLE ------------------------
       when idle =>
         case prog is
-          when "001"  => PE <= progA,
-          when "010"  => PE <= progB,
-          when "011"  => PE <= progC,
-          when "100"  => PE <= progD,
-          when "101"  => PE <= buscando,
-          when others => PE <= EA
+          when "001"  => PE <= progA;
+          when "010"  => PE <= progB;
+          when "011"  => PE <= progC;
+          when "100"  => PE <= progD;
+          when "101"  => PE <= buscando;
+          when "111"  => PE <= zerando;
+          when others => PE <= EA;
         end case;
       -- BUSCANDO -------------------
-      when buscando =>
-          habilita <= '1',
+      when buscando => 
+        if prog = "000" then
+          PE <= EA;
+          else if prog = "111" then
+            PE <= zerando;
+            else if found = '1' then
+              PE <= bloqueio;
+            end if;
+          end if;
+        end if;
       -- PROGS ----------------------
-      when progA => PE <= idle,
-      when progB => PE <= idle,
-      when progC => PE <= idle,
-      when progD => PE <= idle,
+      when progA => 
+        PE <= idle;
+        sel(0) <= '1';
+      when progB => 
+        PE <= idle;
+        sel(1) <= '1';
+      when progC => 
+        PE <= idle;
+        sel(2) <= '1';
+      when progD => 
+        PE <= idle;
+        sel(3) <= '1';
       -- ZERANDO --------------------
       when zerando =>
+          found <= '0';
+          match <= "0000";
+          program <= "0000";
+          sel <= "0000";
+          data <= x"00";
+          PE <= idle;
       -- BLOQUEIO -------------------
       when bloqueio =>
+        case prog is
+          when "111" => PE <= zerando;
+          when "101" => PE <= buscando;
+          when "110" =>
+            alarme <= '0';
+            PE <= buscando;
+          when others => PE <= EA;
+        end case;  
     end case;
   end process decoder_states;
-        
 
   -- SAIDAS
-  alarme <= . . . 
-  dout   <= . . . 
-  numero <=  . . . 
+  dout <= '0' when EA = bloqueio else din;
+  alarme <= '1' when EA = bloqueio else '0';
+  numero <= match when EA = bloqueio else "00";
 
 end architecture;
